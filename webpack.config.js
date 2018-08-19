cc819e8bb1ead516cae025d281abfcf681ad41c0
Matheus Dall'Rosa <matheusdallrosa94@gmail.com>
2018-08-19 09:56:54 -0300

    Add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = (env) => {
   return {
     mode: env.mode,
     output: {
       filename: "index.js",
     },
+    plugins: [
+      new HtmlWebpackPlugin(),
+    ],
   };
 };
